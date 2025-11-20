f751571e96cf57259ec94156354b470158346a01
Tiopp84 <sangdo0804@gmail.com>
2025-11-20 10:18:56 +0700

    update

--- src/main/java/bookingapp/controller/user/Court2.java
+++ src/main/java/bookingapp/controller/user/Court2.java
@@ -1,4 +1,18 @@
 package bookingapp.controller.user;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
 public class Court2 {
+    @FXML private Label lb_header;
+    @FXML private DatePicker fld_date;
+
+    @FXML
+    private void initialize(){
+        lb_header.setText("Đặt Lịch Sân 2");
+        fld_date.setOnAction(e->{
+            fld_date.show();
+        });
+    }
 }
--- src/main/java/bookingapp/controller/user/Court3.java
+++ src/main/java/bookingapp/controller/user/Court3.java
@@ -1,4 +1,18 @@
 package bookingapp.controller.user;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
 public class Court3 {
+    @FXML private Label lb_header;
+    @FXML private DatePicker fld_date;
+
+    @FXML
+    private void initialize(){
+        lb_header.setText("Đặt Lịch Sân 3");
+        fld_date.setOnAction(e->{
+            fld_date.show();
+        });
+    }
 }
